4a1333c66b7a0dc2817e797d2498b9b9271aa6b1
JSTeja3 <saitejajanyavula@gmail.com>
2026-03-01 23:10:47 +0530

    01-03-Problem

---
+++ BruteForce/SplitArrayLargestSum.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class SplitArrayLargestSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] nums = new int[N];
+        for (int i = 0; i < N; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        System.out.println(splitArray(nums, k));
+        sc.close();
+    }
+    public static int splitArray(int[] nums, int k) {
+        int min = 0;
+        int max = 0;
+        int ans = 0;
+        for(int i=0; i<nums.length; i++){
+            min = Integer.max(min, nums[i]);
+            max += nums[i];
+        }
+        for(int i=min; i<=max; i++){
+            if(canSplit(i, nums, k)){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+    public static boolean canSplit(int x, int[] nums, int k){
+        int req = 1;
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(sum > x){
+                req++;
+                sum = nums[i];
+            }
+        }
+        return req<=k;
+    }
+}
